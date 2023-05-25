5ad886447376a8489ebb58c52cb38a1b5b4e9047
Azrylle Unix Enriquez <131931238+AzrylleUnixEnriquez@users.noreply.github.com>
2023-05-25 11:07:35 +0800

    Update U2SpyPlane.java

--- Enriquez_Inheritance/InheritanceMay25/src/MultiLevelInheritance/U2SpyPlane.java
+++ Enriquez_Inheritance/InheritanceMay25/src/MultiLevelInheritance/U2SpyPlane.java
@@ -1,10 +1,11 @@
 package MultiLevelInheritance;
-
+//Plane class inherits the variables and method of vehicle
 public class U2SpyPlane extends Vehicle {
 	
 	String PlaneName = "U-2 Spy Plane";
 	String WingSpan = "31m";
 	
+	//Over writes the STOP method of Vehicle
 	@Override
 	public void STOP() {
 		System.out.println("Touch down");
