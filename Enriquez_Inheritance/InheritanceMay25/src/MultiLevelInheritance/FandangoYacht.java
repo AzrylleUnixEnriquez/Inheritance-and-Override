404c25c458ab0c49684c44c15cdd4d294ce08be2
Azrylle Unix Enriquez <131931238+AzrylleUnixEnriquez@users.noreply.github.com>
2023-05-25 11:09:30 +0800

    Update FandangoYacht.java

--- Enriquez_Inheritance/InheritanceMay25/src/MultiLevelInheritance/FandangoYacht.java
+++ Enriquez_Inheritance/InheritanceMay25/src/MultiLevelInheritance/FandangoYacht.java
@@ -1,10 +1,10 @@
 package MultiLevelInheritance;
-
+//boat class inherits the variables and method of vehicle
 public class FandangoYacht extends Vehicle{
 	
 	String YachtName = "Fandango Yacht";
 	String MainSailColor = "Blue";
-	
+	//Overwrites the STOP method of vehicle
 	public void STOP() {
 		System.out.println("Drop the Anchor");
 		//super.STOP();
