65bd39c61f26f8f5cc1f1ea9d8eda234416ca1c8
Azrylle Unix Enriquez <131931238+AzrylleUnixEnriquez@users.noreply.github.com>
2023-05-25 11:08:18 +0800

    Update ToyotaVios.java

--- Enriquez_Inheritance/InheritanceMay25/src/MultiLevelInheritance/ToyotaVios.java
+++ Enriquez_Inheritance/InheritanceMay25/src/MultiLevelInheritance/ToyotaVios.java
@@ -1,11 +1,11 @@
 package MultiLevelInheritance;
-
+//Car class inherits the variables and method of vehicle
 public class ToyotaVios extends Vehicle {
 
 	String CarName= "Toyota-VIOS";
 	String TireType = "Rubber";
 	
-	
+	//Over writes the method STOP of vehicle
 	@Override
 	public void STOP() {
 		System.out.println("Hit the brake");
